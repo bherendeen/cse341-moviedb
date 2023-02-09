7c2c1fe198a930787dadfeb902a7d5a695a724a2
bherendeen <benjamin.herendeen@gmail.com>
2023-02-09 08:17:26 -0700

    Feb 9 2023 - 8:17am

--- controller/movies.js
+++ controller/movies.js
@@ -2,28 +2,46 @@ const mongodb = require('../db/connection');
 const ObjectId = require('mongodb').ObjectId;
 
 // get all movies
-exports.getAll = async (req, res, next) => {
-    const result = await mongodb.getDb().db('moviedb').collection('movies').find(); // connect to the db and collection. return all movies
-    result.toArray().then((allFoundMovies) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(allFoundMovies);
-    });
+exports.getAll = (req, res, next) => {
+    mongodb
+        .getDb()
+        .db('moviedb')
+        .collection('movies')
+        .find() // connect to the db and collection. return all movies
+        .toArray((err, lists) => {
+            if (err) {
+                res.status(400).json({
+                    message: err
+                });
+            }
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(lists);
+        });
 };
 
 // get a single movie
-exports.getSingle = async (req, res, next) => {
+exports.getSingle = (req, res, next) => {
+    // check if id from params is valid
+    if (!ObjectId.isValid(req.params.userId)) {
+        res.status(400).json('Must use a valid contact id to find a contact.');
+    }
     const movieId = new ObjectId(req.params.movieId); // get the id entered in the url
-    const result = await mongodb
+    mongodb
         .getDb()
         .db('moviedb')
         .collection('movies')
         .find({
             _id: movieId
-        }); // connect to the db and collection. select the movie by id
-    result.toArray().then((singleFoundMovie) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(singleFoundMovie[0]);
-    });
+        }) // connect to the db and collection. select the movie by id
+        .toArray((err, lists) => {
+            if (err) {
+                res.status(400).json({
+                    message: err
+                });
+            }
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(lists);
+        });
 };
 // create movie
 exports.createMovie = async (req, res, next) => {
@@ -79,6 +97,10 @@ exports.createMovie = async (req, res, next) => {
 
 // update movie
 exports.updateMovie = async (req, res, next) => {
+    // check if id from params is valid
+    if (!ObjectId.isValid(req.params.userId)) {
+        res.status(400).json('Must use a valid contact id to find a contact.');
+    }
     const movieId = new ObjectId(req.params.movieId); // get the id entered in the url
     const updateMovie = {
         title: req.body.title,
@@ -133,6 +155,10 @@ exports.updateMovie = async (req, res, next) => {
 
 // delete movie
 exports.deleteMovie = async (req, res, next) => {
+    // check if id from params is valid
+    if (!ObjectId.isValid(req.params.userId)) {
+        res.status(400).json('Must use a valid contact id to find a contact.');
+    }
     const movieId = new ObjectId(req.params.movieId); // get the id entered in the url
     const result = await mongodb
         .getDb()
